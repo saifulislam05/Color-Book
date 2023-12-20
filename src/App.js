9e6ebfd0ad2cb6b2d43027c6d40ed48cb05398d8
saifulislam05 <saiful.bijulia@gmail.com>
2023-12-20 22:38:22 +0530

    feature added: copy to clipboard

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App w-11/12 mx-auto">
       <h1 className='text-2xl font-bold mt-5 mb-3'>Welcome to Color Book</h1>
+      <p className='mb-3 text-green-600'>Click any color to copy the color code!</p>
       <ColorList/>
 
     </div>
--- src/Components/ColorCard.js
+++ src/Components/ColorCard.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ColorCard = (props) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyToClipboard = () => {
+    navigator.clipboard.writeText(props.hex);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+  };
+
   return (
-    <div className="w-32 shadow-md">
+    <div
+      className="w-32 shadow-md cursor-pointer"
+      onClick={handleCopyToClipboard}
+      
+    >
       <div
         className={`w-full h-36`}
         style={{ backgroundColor: props.hex }}
       ></div>
-      <p>{props.hex}</p>
+      <p>{copied ? "Copied!" : props.hex}</p>
       <p>{props.name}</p>
     </div>
   );
